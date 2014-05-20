50f4959e79a91986295869941251b5d11c050c3d
Julien Vehent <julien@linuxwall.info>
2014-05-20 08:23:57 -0400

    updated license on parse_results.py

--- top1m/parse_results.py
+++ top1m/parse_results.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Author: Julien Vehent [:ulfr] - 2013
+# Contributors: Hubert Kario - 2014
+
 from __future__ import division
 
 path = "./results/"
